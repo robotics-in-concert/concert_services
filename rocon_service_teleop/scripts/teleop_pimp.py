8e3fb5533674229d983bf8cc854285d517157b29
Daniel Stonier <d.stonier@gmail.com>
2014-03-18 00:11:53 +0900

    release captured teleopable robots.

--- rocon_service_teleop/scripts/teleop_pimp.py
+++ rocon_service_teleop/scripts/teleop_pimp.py
@@ -52,7 +52,8 @@ class TeleopPimp:
         'teleopable_robots',
         'requester',
         'lock',
-        'pending_requests'  # a list of request id's pending feedback from the scheduler
+        'pending_requests',   # a list of request id's pending feedback from the scheduler
+        'allocated_requests'  # dic of { rocon uri : request id } for captured teleop robots.
     ]
 
     def __init__(self):
@@ -75,6 +76,7 @@ class TeleopPimp:
         self.requester = self.setup_requester(self.service_id)
         self.lock = threading.Lock()
         self.pending_requests = []
+        self.allocated_requests = {}
 
         self.allocate_teleop_service_pair_server = rocon_python_comms.ServicePairServer('capture_teleop', self.ros_capture_teleop_callback, rocon_service_msgs.CaptureTeleopPair, use_threads=True)
         self.allocation_timeout = 5.0  # seconds
@@ -107,10 +109,11 @@ class TeleopPimp:
             self.teleopable_robots[:] = [r for r in self.teleopable_robots if resource.uri != r.uri]
         self.lock.release()
         self.publish_available_teleops()
-        rospy.logwarn("Teleopable robots %s" % self.teleopable_robots)
+        #rospy.logwarn("Teleopable robots %s" % self.teleopable_robots)
 
     def publish_available_teleops(self):
         self.lock.acquire()
+        #rospy.logwarn("Publishing: %s" % [r.status for r in self.teleopable_robots])
         msg = rocon_std_msgs.StringArray()
         msg.strings = [r.uri for r in self.teleopable_robots if r.status != scheduler_msgs.CurrentStatus.ALLOCATED]
         self.available_teleops_publisher.publish(msg)
@@ -119,33 +122,46 @@ class TeleopPimp:
     def ros_capture_teleop_callback(self, request_id, msg):
         '''
          Processes the service pair server 'capture_teleop'. This will run
-         in a thread of its own for each request.
+         in a thread of its own for each request. It has a significantly long lock
+         though - this needs to get fixed.
         '''
         response = rocon_service_msgs.CaptureTeleopResponse()
         response.result = False
         # Todo : request the scheduler for this resource,
         # use self.allocation_timeout to fail gracefully
         self.lock.acquire()
-        if msg.rocon_uri not in [r.uri for r in self.teleopable_robots]:
-            response.result = False
-            self.allocate_teleop_service_pair_server.reply(request_id, response)
-        else:
-            # send a request
-            resource = scheduler_msgs.Resource()
-            resource.id = unique_id.toMsg(unique_id.fromRandom())
-            resource.rapp = 'turtle_concert/teleop'
-            resource.uri = msg.rocon_uri
-            resource_request_id = self.requester.new_request([resource])
-            self.pending_requests.append(resource_request_id)
-            self.requester.send_requests()
-            timeout_time = time.time() + 5.0
-            while not rospy.is_shutdown() and time.time() < timeout_time:
-                if resource_request_id not in self.pending_requests:
-                    response.result = True
-                    break
-                rospy.rostime.wallsleep(0.1)
-            if response.result == False:
-                self.requester.rset[resource_request_id].cancel()
+        if not msg.release:  # i.e. we're capturing:
+            if msg.rocon_uri not in [r.uri for r in self.teleopable_robots]:
+                rospy.logwarn("TeleopPimp : couldn't capture teleopable robot [not available][%s]" % msg.rocon_uri)
+                response.result = False
+                self.allocate_teleop_service_pair_server.reply(request_id, response)
+            else:
+                # send a request
+                resource = scheduler_msgs.Resource()
+                resource.id = unique_id.toMsg(unique_id.fromRandom())
+                resource.rapp = 'turtle_concert/teleop'
+                resource.uri = msg.rocon_uri
+                resource_request_id = self.requester.new_request([resource])
+                self.pending_requests.append(resource_request_id)
+                self.requester.send_requests()
+                timeout_time = time.time() + 5.0
+                while not rospy.is_shutdown() and time.time() < timeout_time:
+                    if resource_request_id not in self.pending_requests:
+                        self.allocated_requests[msg.rocon_uri] = resource_request_id
+                        response.result = True
+                        break
+                    rospy.rostime.wallsleep(0.1)
+                if response.result == False:
+                    rospy.logwarn("TeleopPimp : couldn't capture teleopable robot [timed out][%s]" % msg.rocon_uri)
+                    self.requester.rset[resource_request_id].cancel()
+                else:
+                    rospy.loginfo("TeleopPimp : captured teleopable robot [not available][%s]" % msg.rocon_uri)
+                self.allocate_teleop_service_pair_server.reply(request_id, response)
+        else:  # we're releasing
+            if msg.rocon_uri in self.allocated_requests.keys():
+                self.requester.rset[self.allocated_requests[msg.rocon_uri]].cancel()
+                self.requester.send_requests()
+            response.result = True
             self.allocate_teleop_service_pair_server.reply(request_id, response)
         self.lock.release()
 
