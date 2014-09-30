36b16e32dea5291c412c1462f0d8e7b035b66396
Jihoon Lee <jihoonlee.in@gmail.com>
2014-09-30 17:24:44 +0900

    updates

--- concert_service_make_a_map/scripts/make_a_map_pimp.py
+++ concert_service_make_a_map/scripts/make_a_map_pimp.py
@@ -94,7 +94,7 @@ class MakeAMapPimp(concert_service_utilities.ResourcePimp):
         cmd_vel_remapped = '/' + name + self._default_cmd_vel_topic
         compressed_image_topic_remapped = '/' + name + self._default_compressed_image_topic
         #map_remapped = '/' + name + '/' + self._default_map_topic
-        map_remapped = '/' + self._default_map_topic
+        map_remapped = rospy.get_param('map_topic','/map')
         scan_remapped = '/' + name + self._default_scan_topic
         robot_pose_remapped = '/' + name + '/' + self._default_robot_pose_topic
         save_map_remapped = rospy.get_namespace()  + self._default_save_map_srv
