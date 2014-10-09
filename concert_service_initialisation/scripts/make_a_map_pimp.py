0adf60ac3c8831ce8f2d79e9daa4fdb6899a987d
Jihoon Lee <jihoonlee.in@gmail.com>
2014-10-09 15:04:21 +0900

    remove wc namespace from pimp

--- concert_service_initialisation/scripts/make_a_map_pimp.py
+++ concert_service_initialisation/scripts/make_a_map_pimp.py
@@ -82,8 +82,8 @@ class MakeAMapPimp(concert_service_utilities.ResourcePimp):
         resource.id = unique_id.toMsg(unique_id.fromRandom())
         resource.rapp = self.resource_type
         resource.uri = uri
-        cmd_vel_remapped, compressed_image_topic_remapped, map_remapped, scan_remapped, robot_pose_remapped, wc_namespace_remapped = self._get_remapped_topic(rocon_uri.parse(resource.uri).name.string)
-        resource.remappings = [rocon_std_msgs.Remapping(self._default_cmd_vel_topic, cmd_vel_remapped), rocon_std_msgs.Remapping(self._default_compressed_image_topic, compressed_image_topic_remapped), rocon_std_msgs.Remapping(self._default_map_topic, map_remapped), rocon_std_msgs.Remapping(self._default_scan_topic, scan_remapped), rocon_std_msgs.Remapping(self._default_robot_pose_topic, robot_pose_remapped), rocon_std_msgs.Remapping(self._default_wc_namespace, wc_namespace_remapped)]
+        cmd_vel_remapped, compressed_image_topic_remapped, map_remapped, scan_remapped, robot_pose_remapped = self._get_remapped_topic(rocon_uri.parse(resource.uri).name.string)
+        resource.remappings = [rocon_std_msgs.Remapping(self._default_cmd_vel_topic, cmd_vel_remapped), rocon_std_msgs.Remapping(self._default_compressed_image_topic, compressed_image_topic_remapped), rocon_std_msgs.Remapping(self._default_map_topic, map_remapped), rocon_std_msgs.Remapping(self._default_scan_topic, scan_remapped), rocon_std_msgs.Remapping(self._default_robot_pose_topic, robot_pose_remapped)]
         return resource
 
     def _get_remapped_topic(self, name):
@@ -95,9 +95,8 @@ class MakeAMapPimp(concert_service_utilities.ResourcePimp):
         map_remapped = rospy.get_param('map_topic','/map')
         scan_remapped = '/' + name + self._default_scan_topic
         robot_pose_remapped = '/' + name + '/' + self._default_robot_pose_topic
-        wc_namespace_remapped = rospy.get_param('wc_namespace','/')
 
-        return cmd_vel_remapped, compressed_image_topic_remapped,map_remapped, scan_remapped, robot_pose_remapped, wc_namespace_remapped
+        return cmd_vel_remapped, compressed_image_topic_remapped,map_remapped, scan_remapped, robot_pose_remapped
 
     def loginfo(self, msg):
         rospy.loginfo("MakeAMapPimp : %s"%str(msg))
